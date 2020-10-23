09a57d0630282ac9d9f470240e4b62354faeddcb
jczuchnowski <jakub.czuchnowski@gmail.com>
2020-10-23 11:14:19 +0200

    add execution metrics

--- src/main/scala/zio/zmx/diagnostics/ZMXProtocol.scala
+++ src/main/scala/zio/zmx/diagnostics/ZMXProtocol.scala
@@ -26,35 +26,51 @@ object ZMXProtocol {
   sealed trait Response
 
   object Response {
-    final case class Success(data: Data) extends Response
     final case class Fail(error: Error)  extends Response
+    final case class Success(data: Data) extends Response
   }
 
   sealed trait Message
   sealed trait Command extends Message
 
   object Command {
-    case object Test      extends Command
-    case object FiberDump extends Command
+    case object ExecutionMetrics extends Command
+    case object FiberDump        extends Command
+    case object Test             extends Command
   }
 
   sealed trait Data extends Message
 
   object Data {
-    final case class Simple(message: String) extends Data {
-      override def toString: String = message
+    final case class ExecutionMetrics(metrics: zio.internal.ExecutionMetrics) extends Data {
+      override def toString: String = 
+        render("capacity", metrics.capacity.toString()) + newline +
+        render("concurrency", metrics.concurrency.toString()) + newline +
+        render("dequeued_count", metrics.dequeuedCount.toString()) + newline +
+        render("enqueued_count", metrics.enqueuedCount.toString()) + newline +
+        render("size", metrics.size.toString()) + newline +
+        render("workers_count", metrics.workersCount.toString())
+
+      private val newline = "\n"
+
+      private def render(key: String, value: String): String =
+        s"$key:$value"
     }
 
     final case class FiberDump(dumps: List[String]) extends Data {
       override def toString: String = dumps.mkString("\n")
+    }
+
+    final case class Simple(message: String) extends Data {
+      override def toString: String = message
     }
   }
 
   sealed trait Error extends Message
 
   object Error {
-    case class UnknownCommand(command: String)   extends Error
     case class MalformedRequest(command: String) extends Error
+    case class UnknownCommand(command: String)   extends Error
   }
 
 }

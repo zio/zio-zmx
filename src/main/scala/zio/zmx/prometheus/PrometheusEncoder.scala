c32317f7a925b856fbd39ae76f973b44f1b891ca
Andreas Gies <andreas@wayofquality.de>
2020-11-21 17:51:30 +0100

    Structuring the Prometheus Encoder

--- src/main/scala/zio/zmx/prometheus/PrometheusEncoder.scala
+++ src/main/scala/zio/zmx/prometheus/PrometheusEncoder.scala
@@ -4,17 +4,33 @@ object PrometheusEncoder {
 
   def encode(metrics: List[Metric]): String = metrics.map(encode).mkString("\n")
 
-  private def encode(metric: Metric): String = encodeHead(metric) + (metric.metricType match {
-    case c: MetricType.Counter   => encodeCounter(c)
-    case g: MetricType.Gauge     => encodeGauge(g)
-    case h: MetricType.Histogram => encodeHistogram(h)
-    case s: MetricType.Summary   => encodeSummary(s)
-  })
+  private def encode(metric: Metric): String = {
 
-  private def encodeHead(metric: Metric): String = ???
+    def encodeCounter(c: MetricType.Counter): String     = ???
+    def encodeGauge(g: MetricType.Gauge): String         = ???
+    def encodeHistogram(h: MetricType.Histogram): String = ???
+    def encodeSummary(s: MetricType.Summary): String     = ???
 
-  private def encodeCounter(c: MetricType.Counter): String     = ???
-  private def encodeGauge(g: MetricType.Gauge): String         = ???
-  private def encodeHistogram(h: MetricType.Histogram): String = ???
-  private def encodeSummary(s: MetricType.Summary): String     = ???
+    def encodeHead : String = {
+      val headLines: List[String] = List(
+        s"# TYPE ${metric.name} ${prometheusType(metric.metricType)}"
+      ) ++ metric.help.map(h => s"# HELP $h").toList
+
+      headLines.mkString("", "\n", "\n")
+    }
+
+    def prometheusType(mt: MetricType): String = mt match {
+      case _: MetricType.Counter   => "counter"
+      case _: MetricType.Gauge     => "gauge"
+      case _: MetricType.Histogram => "histogram"
+      case _: MetricType.Summary   => "summary"
+    }
+
+    encodeHead + (metric.metricType match {
+      case c: MetricType.Counter   => encodeCounter(c)
+      case g: MetricType.Gauge     => encodeGauge(g)
+      case h: MetricType.Histogram => encodeHistogram(h)
+      case s: MetricType.Summary   => encodeSummary(s)
+    })
+  }
 }

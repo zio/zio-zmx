abcabbfbaaf606811cec05f9f95ea4fdc0197746
Martin Tjon <martin.tjon@protonmail.com>
2020-11-22 12:31:43 +0100

    Fixed test assertion

--- src/test/scala/zio/zmx/prometheus/PrometheusEncoderSpec.scala
+++ src/test/scala/zio/zmx/prometheus/PrometheusEncoderSpec.scala
@@ -22,15 +22,20 @@ object PrometheusEncoderSpec extends DefaultRunnableSpec {
        */
 
       test("Counter should encode") {
-        val baseCounter = Metric.counter("myCounter", Map("a" -> "b"))
-        val encoded     = PrometheusEncoder.encode(List(baseCounter), None)
-        assert(encoded)(Assertion.equalTo(""))
+
+        val expectedOutput: String = """# TYPE myCounter counter
+                                       |myCounter {a="b"} 0.0 """.stripMargin
+        val baseCounter            = Metric.counter("myCounter", Map("a" -> "b"))
+        val encoded                = PrometheusEncoder.encode(List(baseCounter), None)
+        assert(encoded)(Assertion.equalTo(expectedOutput))
       },
       test("Counter should inc by one") {
-        val baseCounter = Metric.counter("myCounter", Map("a" -> "b"))
-        val incCounter  = baseCounter.inc
-        val encoded     = PrometheusEncoder.encode(List(incCounter), None)
-        assert(encoded)(Assertion.equalTo(""))
+        val expectedOutput: String = """# TYPE myCounter counter
+                                       |myCounter {a="b"} 1.0 """.stripMargin
+        val baseCounter            = Metric.counter("myCounter", Map("a" -> "b"))
+        val incCounter             = baseCounter.inc
+        val encoded                = PrometheusEncoder.encode(List(incCounter), None)
+        assert(encoded)(Assertion.equalTo(expectedOutput))
       } /*,
       test("Counter should Inc by n") {
         for {

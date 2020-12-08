929f978ed10cb44194c39e982f17282eaac537e6
alesavin <alesavin@gmail.com>
2020-12-08 16:50:55 +0100

    cleanup: scalafmt for tests

--- src/test/scala/zio/zmx/diagnostics/ZMXServerSpec.scala
+++ src/test/scala/zio/zmx/diagnostics/ZMXServerSpec.scala
@@ -1,7 +1,7 @@
 package zio.zmx.diagnostics
 
-import zio.{Chunk, ZIO}
-import zio.test.Assertion.{containsString, equalTo}
+import zio.{ Chunk, ZIO }
+import zio.test.Assertion.{ containsString, equalTo }
 import zio.test.{ testM, _ }
 import zio.duration._
 import zio.test.TestAspect.{ nonFlaky, sequential, timeout }
@@ -16,11 +16,10 @@ object ZMXServerSpec extends DefaultRunnableSpec {
   private val openAndCloseServer = server.use(_ => ZIO.unit)
 
   sealed abstract class CliCmd(val in: Chunk[String], val assertion: Assertion[String])
-  case object Test extends CliCmd(Chunk("test"), equalTo("+This is a TEST\r\n"))
+  case object Test    extends CliCmd(Chunk("test"), equalTo("+This is a TEST\r\n"))
   case object Unknown extends CliCmd(Chunk("unknown"), equalTo("-UNKNOWN COMMAND: `unknown`!\r\n"))
-  case object Dump extends CliCmd(Chunk("dump"), equalTo("*0\r\n"))
-  case object Metrics extends CliCmd(Chunk("metrics"),
-    containsString("capacity:") && containsString("concurrency:"))
+  case object Dump    extends CliCmd(Chunk("dump"), equalTo("*0\r\n"))
+  case object Metrics extends CliCmd(Chunk("metrics"), containsString("capacity:") && containsString("concurrency:"))
 
   private val genCliCmd: Gen[Random with Sized, CliCmd] = Gen.oneOf(
     Gen.const[CliCmd](Test),
@@ -43,13 +42,19 @@ object ZMXServerSpec extends DefaultRunnableSpec {
         } yield assertCompletes
       } @@ timeout(5.seconds),
       testM("Is properly answer hundred of commands in parallel 5 threads") {
-        server.use(_ => ZIO.mergeAllParN(5) {
-          Stream.continually(()).take(100).map(_ =>
-            for {
-              cmd <- genCliCmd.runHead.get
-              out <- zmxClient.sendCommand(cmd.in)
-            } yield assert(out)(cmd.assertion))
-        }(assertCompletes)(_ && _))
+        server.use(_ =>
+          ZIO.mergeAllParN(5) {
+            Stream
+              .continually(())
+              .take(100)
+              .map(_ =>
+                for {
+                  cmd <- genCliCmd.runHead.get
+                  out <- zmxClient.sendCommand(cmd.in)
+                } yield assert(out)(cmd.assertion)
+              )
+          }(assertCompletes)(_ && _)
+        )
       } @@ timeout(30.seconds)
     ) @@ sequential @@ nonFlaky
 

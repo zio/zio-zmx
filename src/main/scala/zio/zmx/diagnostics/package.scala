814c9ec2a65aa6670b78a9c9342fa89c8085e8dd
Marek Kadek <kadek.marek@gmail.com>
2021-03-30 09:51:34 +0200

    Bring back Diagnostics

--- src/main/scala/zio/zmx/diagnostics/package.scala
+++ src/main/scala/zio/zmx/diagnostics/package.scala
@@ -9,6 +9,8 @@ import com.github.ghik.silencer.silent
 
 package object diagnostics {
 
+  type Diagnostics = Has[Diagnostics.Service]
+
   val ZMXSupervisor: Supervisor[Set[Fiber.Runtime[Any, Any]]] =
     new Supervisor[Set[Fiber.Runtime[Any, Any]]] {
 
@@ -35,4 +37,49 @@ package object diagnostics {
         Propagation.Continue
       }
     }
+
+  type Diagnostics = Has[Diagnostics.Service]
+
+  object Diagnostics {
+    trait Service {}
+
+    /**
+     * The Diagnostics service will listen on the specified port for commands to perform fiber
+     * dumps, either across all fibers or across the specified fiber ids.
+     */
+    def live(host: String, port: Int): ZLayer[ZEnv, Throwable, Diagnostics] =
+      ZLayer.fromManaged(
+        ZMXServer
+          .make(ZMXConfig(host, port, true))
+          .as(new Service {})
+      )
+  }
+
+  // TODO Does this needs to be part of ZIO-Core?
+  // type CoreMetrics = Has[CoreMetrics.Service]
+
+// TODO: Implement some core ZIO metrics
+//   object CoreMetrics {
+//     trait Service {
+//       def enable: UIO[Unit]
+//
+//       def disable: UIO[Unit]
+//
+//       def isEnabled: UIO[Boolean]
+//     }
+//
+//     val enable: ZIO[CoreMetrics, Nothing, Unit] = ZIO.accessM[CoreMetrics](_.get.enable)
+//
+//     val disable: ZIO[CoreMetrics, Nothing, Unit] = ZIO.accessM[CoreMetrics](_.get.disable)
+//
+//     val isEnabled: ZIO[CoreMetrics, Nothing, Boolean] = ZIO.accessM[CoreMetrics](_.get.isEnabled)
+//
+//     /**
+//      * The `CoreMetrics` service installs hooks into ZIO runtime system to track
+//      * important core metrics, such as number of fibers, fiber status, fiber
+//      * lifetimes, etc.
+//      */
+//     def live: ZLayer[Metrics, Nothing, CoreMetrics] = ???
+//   }
+
 }

9481263ff0a5e6ba8f84a8ef699df4e8fbfd30b1
Scala Steward <me@scala-steward.org>
2021-05-28 16:15:19 +0200

    Update scala-library to 2.12.14

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,7 +4,7 @@ import sbtbuildinfo._
 import BuildInfoKeys._
 
 object BuildHelper {
-  private val Scala212 = "2.12.12"
+  private val Scala212 = "2.12.14"
   private val Scala213 = "2.13.4"
 
   private val stdOptions = Seq(

8d6de13fc1885c40b7c66290c8c89e2391fa9da2
Andreas Gies <andreas@wayofquality.de>
2021-07-05 13:59:07 +0200

    Finalize config to make src maps work for the client development

--- client/js/vite.config.js
+++ client/js/vite.config.js
@@ -11,11 +11,23 @@ export default ({ mode }) => {
     const mainJS = `./target/scala-${scalaVersion}/${appDir}/main.js`
     const script = `<script type="module" src="./target/rollup/zio-zmx-client-app.js"></script>`
 
+    // We need the project directory, do we can sourcemap our own project sources during development
+    const prjDir = path.resolve("../..");
+
     const BreakException = {}
 
     const sourcePathMappings = [
+        // The first settings correct sourcemaps that have accidentally been published with incorrect code references 
+        // and remaps those to vaild githup raw urls. 
+        // It seems that changes to these mappings require a build rather than running just the vite server 
         { from: "https:/raw", to: "https://raw" },
-        { from: "file:/home/runner/work/animus/animus", to: "https://raw.githubusercontent.com/kitlangton/animus/v0.1.9" }
+        { from: "file:/home/runner/work/animus/animus", to: "https://raw.githubusercontent.com/kitlangton/animus/v0.1.9" },
+        { from: "file:/home/runner/work/zio/zio", to: "https://raw.githubusercontent.com/zio/zio/v1.0.9" },
+        // We have to remap the client sources, so that the vite root directory does not collide with any of the 
+        // directories containing the Scala sources, otherwise vite cant serve the dev page
+        { from: 'file:' + prjDir + '/client/js/src', to: "/cltjs" },
+        { from: 'file:' + prjDir + '/client/shared/src', to: "/cltshared" },
+        { from: 'file:' + prjDir, to: "" }
     ]
 
     return {
@@ -31,27 +43,28 @@ export default ({ mode }) => {
                     dir: 'target/rollup',
                     entryFileNames: 'zio-zmx-client-[name].js',
                     format: 'es',
-                    sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+                    sourcemapPathTransform: (relativeSourcePath, _sourcemapPath) => {
                         // will replace relative paths with absolute paths
                         var srcPath = relativeSourcePath.replace(`../scala-${scalaVersion}/${appDir}/`, '');
                         var result = srcPath
-                        try {
-                            sourcePathMappings.forEach((value, idx, array) => {
-                                if (result.startsWith(value.from)) {
-                                    result = result.replace(value.from, value.to);
-                                    throw BreakException;
-                                }
-                            })
-                        } catch (e) {
-                            if (e != BreakException) throw e
-                        }
-                        console.log(result + " -- " + srcPath);
+                        var mapped = false
+                        sourcePathMappings.forEach((value, _idx, _array) => {
+                            if (!mapped && result.startsWith(value.from)) {
+                                mapped = true
+                                result = result.replace(value.from, value.to);
+                            }
+                        })
+                        console.log(result)
                         return result
                     },
                 }
             }
         },
         server: {
+            fs: {
+                strict: false,
+                allow: ['../..']
+            },
             proxy: {
                 '/api': {
                     target: 'http://localhost:8088',
@@ -60,7 +73,7 @@ export default ({ mode }) => {
                 },
             }
         },
-        publicDir: './src/main/static/public',
+        publicDir: 'public',
         plugins: [
             ...(process.env.NODE_ENV === 'production' ? [minifyHtml(),] : []),
             injectHtml({
@@ -71,6 +84,7 @@ export default ({ mode }) => {
         ],
         resolve: {
             alias: {
+                '/@': prjDir,
                 'stylesheets': path.resolve(__dirname, './frontend/src/main/static/stylesheets'),
             }
         }

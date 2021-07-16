db9b962bf6c335d6d62617b19dcb962f190f0cd0
Andreas Gies <andreas@wayofquality.de>
2021-07-16 18:03:05 +0200

    More work on displaying time related Graphs

--- client/js/src/main/scala/zio/zmx/client/frontend/views/ChartView.scala
+++ client/js/src/main/scala/zio/zmx/client/frontend/views/ChartView.scala
@@ -4,6 +4,8 @@ import scalajs.js
 import org.scalajs.dom
 import scalajs.js.annotation.JSImport
 import com.raquo.laminar.api.L._
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 object ChartView {
 
@@ -11,30 +13,69 @@ object ChartView {
   @JSImport("chart.js/auto", JSImport.Default)
   class Chart(ctx: dom.Element, config: js.Dynamic) extends js.Object {}
 
+  ScalaDateAdapter.install()
+
   def create(): HtmlElement = {
+
+    val now  = Instant.now()
+    val day1 = now.minus(1L, ChronoUnit.DAYS)
+    val day2 = now.minus(2L, ChronoUnit.DAYS)
 
     val dataset1 = js.Dynamic.literal(
       label = "demo",
-      data = js.Array(2, 4, 6),
-      backgroundColor = "#00DD00"
+      data = js.Array(
+        js.Dynamic.literal(x = day2.toEpochMilli().doubleValue(), y = 2),
+        js.Dynamic.literal(x = day1.toEpochMilli().doubleValue(), y = 4),
+        js.Dynamic.literal(x = now.toEpochMilli().doubleValue(), y = 6)
+      ),
+      borderColor = "#00DD00",
+      fill = false,
+      tension = 0.6
     )
 
     val dataset2 = js.Dynamic.literal(
       label = "values",
-      data = js.Array(3, 4, 5),
-      backgroundColor = "#0000DD"
+      data = js.Array(
+        js.Dynamic.literal(x = (day2.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 3),
+        js.Dynamic.literal(x = (day1.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 7),
+        js.Dynamic.literal(x = (now.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 1)
+      ),
+      borderColor = "#3000DD",
+      fill = false,
+      tension = 0.6
+    )
+
+    val dataset3 = js.Dynamic.literal(
+      label = "something",
+      data = js.Array(
+        js.Dynamic.literal(x = (day2.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 8),
+        js.Dynamic.literal(x = (day1.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 5),
+        js.Dynamic.literal(x = (now.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 9)
+      ),
+      borderColor = "#DD0000",
+      fill = false,
+      tension = 0.6
     )
 
     val config = js.Dynamic.literal(
-      `type` = "bar",
+      `type` = "line",
+      options = js.Dynamic.literal(
+        parsing = false,
+        maintainAspectRatio = false,
+        scales = js.Dynamic.literal(
+          x = js.Dynamic.literal(
+            `type` = "timeseries"
+          )
+        )
+      ),
       data = js.Dynamic.literal(
-        labels = js.Array("Foo", "Bar", "Baz"),
-        datasets = js.Array(dataset1, dataset2)
+        // labels = js.Array("Foo", "Bar", "Baz"),
+        datasets = js.Array(dataset1, dataset2, dataset3)
       )
     )
 
     div(
-      cls := "bg-gray-900 text-gray-50 rounded my-3 p-3 h-60 flex",
+      cls := "bg-gray-900 text-gray-50 rounded my-3 p-3 h-80 flex",
       div(
         cls := "w-1/2 h-full rounded bg-gray-50 p-3",
         div(

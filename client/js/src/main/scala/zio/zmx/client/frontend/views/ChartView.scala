e3311dcbc08b5a86f9c9bcc3ef3dbad4a221b732
Andreas Gies <andreas@wayofquality.de>
2021-07-19 14:22:08 +0200

    Some more work on dynamic diagram updates

--- client/js/src/main/scala/zio/zmx/client/frontend/views/ChartView.scala
+++ client/js/src/main/scala/zio/zmx/client/frontend/views/ChartView.scala
@@ -2,79 +2,113 @@ package zio.zmx.client.frontend.views
 
 import scalajs.js
 import org.scalajs.dom
+import org.scalajs.dom.html.Canvas
 import scalajs.js.annotation.JSImport
 import com.raquo.laminar.api.L._
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import com.raquo.laminar.nodes.ReactiveHtmlElement
+import scala.collection.mutable
+import scala.util.Random
+
+@js.native
+@JSImport("chart.js/auto", JSImport.Default)
+class Chart(ctx: dom.Element, config: js.Dynamic) extends js.Object {
+  def update(mode: js.UndefOr[String]): Unit = js.native
+}
+
+final case class ChartView(
+  canvas: ReactiveHtmlElement[Canvas]
+) {
+
+  private val series: mutable.Map[String, TimeSeries] = mutable.Map.empty
+
+  private val options: js.Dynamic = js.Dynamic.literal(
+    `type` = "line",
+    options = js.Dynamic.literal(
+      parsing = false,
+      maintainAspectRatio = false,
+      scales = js.Dynamic.literal(
+        x = js.Dynamic.literal(
+          `type` = "timeseries"
+        )
+      )
+    ),
+    data = js.Dynamic.literal(
+      datasets = js.Array[js.Dynamic]()
+    )
+  )
+
+  def addTimeseries(ts: TimeSeries): Unit =
+    if (series.get(ts.key).isEmpty) {
+      val _ = series.put(ts.key, ts)
+      chart
+        .asInstanceOf[js.Dynamic]
+        .selectDynamic("data")
+        .selectDynamic("datasets")
+        .asInstanceOf[js.Array[js.Dynamic]]
+        .push(ts.asDataSet)
+      update()
+    }
+
+  def recordData(key: String, when: Instant, value: Double): Unit = {
+    series.get(key).foreach(ts => ts.recordData(when, value))
+    update()
+  }
+
+  def update(): Unit =
+    chart.update(())
+
+  lazy val chart = new Chart(canvas.ref, options)
+}
+
+final case class TimeSeries(
+  label: String,
+  color: String,
+  key: String,
+  tension: Double = 0,
+  data: js.Array[js.Dynamic] = js.Array(),
+  maxSize: Int = 100
+) {
+  def recordData(when: Instant, value: Double) = {
+    val xx  = when.toEpochMilli().doubleValue()
+    println(s"($xx, $value)")
+    val cnt = data.push(
+      js.Dynamic.literal(
+        x = xx,
+        y = value
+      )
+    )
+    println(s"TimeSeries [$key] has now [$cnt] values. $dumpData")
+  }
+
+  private def dumpData: String =
+    data.map { entry =>
+      val x = entry.selectDynamic("x").asInstanceOf[Double]
+      val y = entry.selectDynamic("y").asInstanceOf[Double]
+      s"[($x, $y)]"
+    }.mkString(",")
+
+  def asDataSet: js.Dynamic = {
+    println(s"Current data pushed to Chart is $dumpData")
+    js.Dynamic.literal(
+      label = label,
+      borderColor = color,
+      fill = false,
+      tension = tension,
+      data = data
+    )
+  }
+}
 
 object ChartView {
 
-  @js.native
-  @JSImport("chart.js/auto", JSImport.Default)
-  class Chart(ctx: dom.Element, config: js.Dynamic) extends js.Object {}
-
-  ScalaDateAdapter.install()
+  private lazy val adapterInstalled = ScalaDateAdapter.install()
 
   def create(): HtmlElement = {
+    val _ = adapterInstalled
 
-    val now  = Instant.now()
-    val day1 = now.minus(1L, ChronoUnit.DAYS)
-    val day2 = now.minus(2L, ChronoUnit.DAYS)
-
-    val dataset1 = js.Dynamic.literal(
-      label = "demo",
-      data = js.Array(
-        js.Dynamic.literal(x = day2.toEpochMilli().doubleValue(), y = 2),
-        js.Dynamic.literal(x = day1.toEpochMilli().doubleValue(), y = 4),
-        js.Dynamic.literal(x = now.toEpochMilli().doubleValue(), y = 6)
-      ),
-      borderColor = "#00DD00",
-      fill = false,
-      tension = 0.6
-    )
-
-    val dataset2 = js.Dynamic.literal(
-      label = "values",
-      data = js.Array(
-        js.Dynamic.literal(x = (day2.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 3),
-        js.Dynamic.literal(x = (day1.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 7),
-        js.Dynamic.literal(x = (now.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 1)
-      ),
-      borderColor = "#3000DD",
-      fill = false,
-      tension = 0.6
-    )
-
-    val dataset3 = js.Dynamic.literal(
-      label = "something",
-      data = js.Array(
-        js.Dynamic.literal(x = (day2.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 8),
-        js.Dynamic.literal(x = (day1.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 5),
-        js.Dynamic.literal(x = (now.minus(1L, ChronoUnit.DAYS)).toEpochMilli().doubleValue(), y = 9)
-      ),
-      borderColor = "#DD0000",
-      fill = false,
-      tension = 0.6
-    )
-
-    val config = js.Dynamic.literal(
-      `type` = "line",
-      options = js.Dynamic.literal(
-        parsing = false,
-        maintainAspectRatio = false,
-        scales = js.Dynamic.literal(
-          x = js.Dynamic.literal(
-            `type` = "timeseries"
-          )
-        )
-      ),
-      data = js.Dynamic.literal(
-        // labels = js.Array("Foo", "Bar", "Baz"),
-        datasets = js.Array(dataset1, dataset2, dataset3)
-      )
-    )
-
-    div(
+    val element = div(
       cls := "bg-gray-900 text-gray-50 rounded my-3 p-3 h-80 flex",
       div(
         cls := "w-1/2 h-full rounded bg-gray-50 p-3",
@@ -86,7 +120,7 @@ object ChartView {
               width("100%"),
               height("100%"),
               onMountCallback { el =>
-                val _ = new Chart(el.thisNode.ref, config)
+                val _ = new ChartView(el.thisNode).chart
               }
             )
           )
@@ -100,5 +134,7 @@ object ChartView {
         )
       )
     )
+
+    element
   }
 }

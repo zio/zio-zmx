00cced5ccc98018b4eed3142080b5dbf771321bf
Scala Steward <me@scala-steward.org>
2021-11-02 04:40:13 +0100

    Update scala-library to 2.13.7

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import BuildInfoKeys._
 object BuildHelper {
   val Scala211   = "2.11.12"
   val Scala212   = "2.12.14"
-  val Scala213   = "2.13.6"
+  val Scala213   = "2.13.7"
   val ScalaDotty = "3.0.2"
 
   private val stdOptions = Seq(

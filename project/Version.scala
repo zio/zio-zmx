7477e188d5cb7b3f0728f01749201c58b1954508
Andreas Gies <andreas@wayofquality.de>
2022-03-17 11:50:33 +0100

    Migrating the Prometheus encoding to ZIO 2 RC3

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object Version {
   val Scala213   = "2.13.8"
   val ScalaDotty = "3.1.0"
 
-  val zio       = "2.0.0-RC2"
+  val zio       = "2.0.0-RC2+97-b9d2fd51-SNAPSHOT"
   val zioHttp   = "2.0.0-RC4"
   val animus    = "0.1.12"
   val fansi     = "0.2.14"

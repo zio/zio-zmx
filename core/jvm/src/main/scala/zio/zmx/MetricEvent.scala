3678fb581a9fe5193127674a88063c7587588658
Andreas Gies <andreas@wayofquality.de>
2022-04-22 10:07:34 +0200

    Pulling in MetricEvent update

--- core/jvm/src/main/scala/zio/zmx/MetricEvent.scala
+++ core/jvm/src/main/scala/zio/zmx/MetricEvent.scala
@@ -2,6 +2,7 @@ package zio.zmx
 
 import java.time.Instant
 
+import zio._
 import zio.metrics._
 
 sealed trait MetricEvent
@@ -18,4 +19,49 @@ object MetricEvent {
     newState: MetricState.Untyped,
     timestamp: Instant)
       extends MetricEvent
+
+  def make[Type <: MetricKeyType { type Out = Out0 }, Out0](
+    metricKey: MetricKey[Type],
+    oldState: Option[MetricState[Out0]],
+    newState: MetricState[Out0],
+  ): ZIO[Any, IllegalArgumentException, MetricEvent] =
+    (oldState, newState) match {
+      case (Some(oldState @ MetricState.Counter(oldCount)), newState @ MetricState.Counter(newCount))               =>
+        if (oldCount != newCount)
+          ZIO.succeed(Updated(metricKey, oldState, newState, Instant.now))
+        else
+          ZIO.succeed(Unchanged(MetricPair.unsafeMake(metricKey, newState), Instant.now))
+      case (Some(oldState @ MetricState.Gauge(oldValue)), newState @ MetricState.Gauge(newValue))                   =>
+        if (oldValue != newValue)
+          ZIO.succeed(Updated(metricKey, oldState, newState, Instant.now))
+        else
+          ZIO.succeed(Unchanged(MetricPair.unsafeMake(metricKey, newState), Instant.now))
+      case (Some(oldState @ MetricState.Frequency(oldOccurences)), newState @ MetricState.Frequency(newOcurrences)) =>
+        if (oldOccurences != newOcurrences)
+          ZIO.succeed(Updated(metricKey, oldState, newState, Instant.now))
+        else
+          ZIO.succeed(Unchanged(MetricPair.unsafeMake(metricKey, newState), Instant.now))
+      case (
+            Some(oldState @ MetricState.Summary(_, _, oldCount, _, _, _)),
+            newState @ MetricState.Summary(_, _, newCount, _, _, _),
+          ) =>
+        if (oldCount != newCount)
+          ZIO.succeed(Updated(metricKey, oldState, newState, Instant.now))
+        else
+          ZIO.succeed(Unchanged(MetricPair.unsafeMake(metricKey, newState), Instant.now))
+      case (
+            Some(oldState @ MetricState.Histogram(_, oldCount, _, _, _)),
+            newState @ MetricState.Histogram(_, newCount, _, _, _),
+          ) =>
+        if (oldCount != newCount)
+          ZIO.succeed(Updated(metricKey, oldState, newState, Instant.now))
+        else
+          ZIO.succeed(Unchanged(MetricPair.unsafeMake(metricKey, newState), Instant.now))
+      case (None, state)                                                                                            =>
+        ZIO.succeed(New(MetricPair.unsafeMake(metricKey, state), Instant.now))
+      case (oldState, newState)                                                                                     =>
+        ZIO.fail(new IllegalArgumentException(s"Unsupported MetricState combination: $oldState, $newState"))
+
+    }
+
 }
